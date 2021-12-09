58e8cb7a1fb1d2451ced561023528d5e23f323cc
Coimbrax <lucascoimbralago@gmail.com>
2021-12-09 10:08:09 -0300

    Exercício_4

--- modulo1/resolvendo-problemas/lista-revisao-js/exercicios.js
+++ modulo1/resolvendo-problemas/lista-revisao-js/exercicios.js
@@ -26,7 +26,13 @@ function retornaArrayOrdenado(array) {
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+  const arrayPar = []
+  for (let i = 0; i < array.length; i++){
+    if (array[i] % 2 === 0){
+      arrayPar.push(array[i])
+    }
+  }
+  return arrayPar
 }
 
 // EXERCÍCIO 05
