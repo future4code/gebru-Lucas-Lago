1672080d1eacd30ac6640c86eccece5a7cd82a74
Coimbrax <lucascoimbralago@gmail.com>
2021-12-10 10:06:50 -0300

    Exercício_15A

--- modulo1/resolvendo-problemas/lista-revisao-js/exercicios.js
+++ modulo1/resolvendo-problemas/lista-revisao-js/exercicios.js
@@ -148,12 +148,16 @@ function retornaPessoasNaoAutorizadas(pessoas) {
 
 // EXERCÍCIO 14
 function retornaContasComSaldoAtualizado(contas) {
-
+    
+  
 }
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+  consultas.sort(function(a, b){
+    return a.nome > b.nome ? 1 : b.nome > a.nome ? -1 : 0
+  })
+  return consultas
 }
 
 // EXERCÍCIO 15B
