0c19336721b46b3866d6c63b7da218bd8acb21b2
Coimbrax <lucascoimbralago@gmail.com>
2022-05-19 10:17:29 -0300

    Exercícios de Quinta-feira - Node e Package.json

---
+++ modulo5/node-package-json/index.js
@@ -0,0 +1,49 @@
+// EXERCÍCIO 1 
+    // a. Utilizamos o comando process.argv[i], sendo que i tem que ser maior ou igual a 2 (2, 3, 4 ...). 
+    // Lembrando que os valores de i = 1 é reservado ao node e i =2 é reservado ao arquivo index.js!
+    
+    // b.
+        // const nome = process.argv[2]
+        // const idade = process.argv[3]
+
+        // console.log(`Olá, ${nome}! Você tem ${idade} anos.`)
+
+    // c.
+        // const nome = process.argv[2]
+        // const idade = process.argv[3]
+        // const novaIdade = Number(idade) + 7
+
+        // console.log(`Olá, ${nome}! Você tem ${idade}. Em sete anos você terá ${novaIdade}`)
+
+
+// EXERCÍCIO 2
+        // const operador = process.argv[2]
+        // const num1 = Number(process.argv[3])
+        // const num2 = Number(process.argv[4])
+
+        // switch(operador){
+        //     case "soma":
+        //         (resposta = num1 + num2)
+        //         break
+        //     case "sub":
+        //         (resposta = num1 - num2)
+        //         break
+        //     case "mult":
+        //         (resposta = num1 * num2)
+        //     case "div":
+        //         resposta = num1 / num2
+        //         break
+        //     default:
+        //         ("Operador inválido")
+        // }
+
+        // console.log(`Resposta: ${resposta}`)
+
+
+// EXERCÍCIO 3
+    // const novaTarefa = process.argv[2]
+    // const listaDeTarefas = []
+
+    // listaDeTarefas.push(novaTarefa)
+
+    // console.log(listaDeTarefas)
