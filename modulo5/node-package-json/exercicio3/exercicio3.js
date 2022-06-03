cbcde437be6b990a82aa6325192fe22e6287598d
Coimbrax <lucascoimbralago@gmail.com>
2022-06-03 15:58:35 -0300

    Exercícios node-package-json

---
+++ modulo5/node-package-json/exercicio3/exercicio3.js
@@ -0,0 +1,13 @@
+// Exercício 3
+
+const novaTarefa1 = process.argv[2]
+const novaTarefa2 = process.argv[3]
+const novaTarefa3 = process.argv[4]
+
+const listTarefa = []
+
+listTarefa.push(novaTarefa1)
+listTarefa.push(novaTarefa2)
+listTarefa.push(novaTarefa3)
+
+console.log(listTarefa)
