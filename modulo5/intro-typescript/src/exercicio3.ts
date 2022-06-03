ca34e02bc74017c294dbff0e7247aac6051e42f6
Coimbrax <lucascoimbralago@gmail.com>
2022-06-03 17:35:07 -0300

    Exercícios intro-typescript

--- modulo5/intro-typescript/src/exercicio3.ts
+++
@@ -1,8 +0,0 @@
-export function exercicio3(
-    ano: number
-) :boolean {
-    const cond1: boolean = ano % 400 === 0;
-    const cond2: boolean = (ano % 4 === 0) && (ano % 100 !== 0)
-    
-    return (cond1 || cond2)
-}
